0480c4b0605aa5352570329dc34330cbb6fbdf3f
nhattien <nhattien1245@gmail.com>
2023-05-24 00:53:08 +0700

    Nearby and Popular jobs 90%, search feature done, app is quite slow

--- app/index.js
+++ app/index.js
@@ -8,20 +8,20 @@ import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components
 const Home = () => {
 
   const router = useRouter();
-
+  const [searchTerm, setSearchTerm] = useState("");
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
-      <Stack
+      <Stack.Screen
         options={{
           headerStyle: { backgroundColor: COLORS.lightWhite },
           headerShadowVisible: false,
-          headerLeft: () => {
-            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
-          },
-          headerRight: () => {
-            <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
-          },
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension='60%' />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension='100%' />
+          ),
           headerTitle: ""
         }}
       />
@@ -34,10 +34,17 @@ const Home = () => {
           }}
         >
           <Welcome
-
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
+            handleClick={() => {
+              if(searchTerm) {
+                router.push(`/search/${searchTerm}`)
+              }
+            }}
           />
 
           <Popularjobs />
+
           <Nearbyjobs />
         </View>
       </ScrollView>
